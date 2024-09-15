528b0d57d3bab71f6084f086ad70b5961957979d
Justin Wu <justinytwu@gmail.com>
2024-09-16 00:45:07 +0800

    forgot to commit - update Machine interface

--- src/types.ts
+++ src/types.ts
@@ -14,6 +14,7 @@ export interface Machine {
   location: [number, number, number]; // [floor, x, y]
   currentWorkoutLogId: string | null;
   queueSize: number;
+  maximumQueueSize: number;
 }
 
 export interface QueueItem {
